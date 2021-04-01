b0b0917566f17c7b7a8e092e918990dc51eef082
wzc <wzc>
2021-04-01 23:43:34 +0800

    4.1家里

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,23 +1,48 @@
 import React, {Component} from 'react';
-import {Button, message} from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './login.less';
 import logo from './images/logo.png';
 
 /* 登录的路由组件 */
 class Login extends Component {
 
-  handleClick = () => { message.success('成功啦...'); }
-
   render() {
     return (
         <div className='login'>
-          <Button type='primary' onClick={this.handleClick}>学习</Button>
           <header className='login-header'>
             <img src={logo} alt='headerImg'/>
-            <h2>后台管理系统</h2>
+            <h1>后台管理系统</h1>
           </header>
           <section className='login-content'>
-
+            <h2>用户登录</h2>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{ remember: true }}
+            >
+              <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: '请输入用户名!' }]}
+              >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+              </Form.Item>
+              <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: '请输入密码!' }]}
+              >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="密码"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  登录
+                </Button>
+              </Form.Item>
+            </Form>
           </section>
         </div>
     );
